edb0335c6ecd5b2757594ca2748b804d5c5e6a2c
renish_dsouza <renish_b231198cs@nitc.ac.in>
2025-09-01 02:04:32 +0530

    stage_2 is working running all fine

--- mynitcbase/Buffer/BlockBuffer.cpp
+++ mynitcbase/Buffer/BlockBuffer.cpp
@@ -2,3 +2,61 @@
 
 #include <cstdlib>
 #include <cstring>
+
+// the declarations for these functions can be found in "BlockBuffer.h"
+
+BlockBuffer:: BlockBuffer(int blockNum){
+    //initialise thi.blockNum with the argument
+    this->blockNum=blockNum;
+}
+
+//calls the [arent class constructor
+RecBuffer:: RecBuffer(int blockNum) : BlockBuffer(blockNum){}
+
+//load the block header into the argument pointer
+int BlockBuffer::getHeader(struct HeadInfo *head){
+    unsigned char buffer[BLOCK_SIZE];
+
+    //read the block at this.blockNum into the buffer
+    Disk :: readBlock(buffer, this->blockNum);
+
+    //populate the numEntries, numAttrs and numSlots fields in*head
+    memcpy(&head->numSlots,buffer+24,4);
+    memcpy(&head->numEntries,buffer+16,4);
+    memcpy(&head->numAttrs,buffer+20,4);
+    memcpy(&head->rblock,buffer+12,4);
+    memcpy(&head->lblock,buffer+8,4);
+
+    return SUCCESS;
+}
+
+//load the record at slotNum into the argument pointer
+int RecBuffer::getRecord(union Attribute *rec,int slotNum){
+    struct HeadInfo head;
+    this->getHeader(&head);
+    // get the header using this.getHeader() function
+
+    int attrCount = head.numAttrs;
+    int slotCount = head.numSlots;
+
+    //read the block at this.blockNum into  a buffer
+    unsigned char buffer[BLOCK_SIZE];
+
+    Disk::readBlock(buffer, this->blockNum);
+
+    /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
+        - each record will have size attrCount * ATTR_SIZE
+        - slotMap will be of sisze slotCount
+    */
+
+    int recordSize = attrCount * ATTR_SIZE;
+
+    int offset = HEADER_SIZE + slotCount + (recordSize * slotNum);
+
+    // unsigned char *slotPointer = &buffer/* calculate buffer + offset */;
+
+    // load the record into the rec data structure
+    memcpy(rec, buffer + offset, recordSize);
+
+    return SUCCESS;
+}
--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -2,6 +2,10 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+using namespace std;
 
 int main(int argc, char *argv[]) {
   /* Initialize the Run Copy of Disk */
@@ -9,5 +13,41 @@ int main(int argc, char *argv[]) {
   // StaticBuffer buffer;
   // OpenRelTable cache;
 
-  return FrontendInterface::handleFrontend(argc, argv);
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  // struct HeadInfo* relCatHeader=(struct HeadInfo*)malloc(sizeof(struct HeadInfo));
+  //struct HeadInfo* attrCatHeader=(struct HeadInfo*)malloc(sizeof(struct HeadInfo));
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for(int i=0;i<relCatHeader.numEntries;i++){
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS];// will store the record ffrom the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for(int j=0;j<attrCatHeader.numEntries;j++){
+      // declare attrCatRecord and load the attribute catalog entry into it
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord,j);
+      if(strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal, attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal) == 0){
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal ==NUMBER ? "NUM" : "STR";
+        printf(" %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    }
+    printf("\n");
+  }
+  return 0;
+
+  // return FrontendInterface::handleFrontend(argc, argv);
 }
